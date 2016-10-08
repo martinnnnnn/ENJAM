40684c9182d12c37a693fa2750b1dba534b9f501
Soufian <soufiazzouz@laposte.net>
2016-10-08 14:20:55 +0200

    unlimited generation + player controls

---
+++ Balayeur_Simulator/Assets/Scripts/Flower.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flower : MonoBehaviour
+{
+    [SerializeField]
+    private float m_fTimeBeforeDestruction;
+
+    [SerializeField]
+    private float m_fLinearDragMinBound;
+
+    [SerializeField]
+    private float m_fLinearDragMaxBound;
+
+    // Use this for initialization
+    void Start ()
+    {
+        GetComponent<Rigidbody2D>().drag = Random.Range(m_fLinearDragMinBound, m_fLinearDragMaxBound);
+        //Destroy(gameObject, m_fTimeBeforeDestruction);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
---
+++ Balayeur_Simulator/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour
+{
+    //[SerializeField]
+    private float m_fTranslation;
+
+    [SerializeField]
+    private float m_fSpeed;
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        m_fTranslation = Input.GetAxis("Horizontal") * m_fSpeed;
+        m_fTranslation *= Time.deltaTime;
+        transform.Translate(m_fTranslation, 0f, 0f);
+    }
+}
