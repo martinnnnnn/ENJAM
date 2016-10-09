dc4f12a06a0c5e13861d26314859d80063ec7912
martinnnnnn <martin.penard@gmail.com>
2016-10-09 05:40:57 +0200

    startup scene finie (presque) + ajout sprite PLS & Jean Pierre

---
+++ Balayeur_Simulator/Assets/Scripts/TheScriptOfTheButon.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheScriptOfTheButon : MonoBehaviour {
+
+	
+
+
+    public void LoadScene()
+    {
+        Application.LoadLevel("test_sou");
+    }
+}
